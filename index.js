a2389c4528326f4a5d97b289147a46a65d9c7d14
ManuelAOrtiz <devolthdd@hotmail.com>
2018-09-21 16:57:34 -0400

    FirstCommit

---
+++ index.js
@@ -0,0 +1,12 @@
+ Volume in drive D has no label.
+ Volume Serial Number is 0B8B-16F9
+
+ Directory of D:\Class\Assignments\DomChallenge
+
+09/21/2018  04:55 PM    <DIR>          .
+09/21/2018  04:55 PM    <DIR>          ..
+09/21/2018  04:55 PM               409 index.css
+09/21/2018  04:55 PM               359 index.html
+09/21/2018  04:55 PM                 0 index.js
+               3 File(s)            768 bytes
+               2 Dir(s)  992,023,048,192 bytes free
