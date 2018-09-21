d8c302dc0087d44a389ad6fcb3855da7d4d55d53
ManuelAOrtiz <devolthdd@hotmail.com>
2018-09-21 17:30:40 -0400

    End of day

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
- Volume in drive D has no label.
- Volume Serial Number is 0B8B-16F9
+var buttonOne = document.GetElementById("buttonOne");
+var buttonTwo = document.GetElementById("buttonTwo");
+var buttonOneText = document.GetElementById("buttonOneText");
+var buttonTwoText = document.GetElementById("buttonTwoText");
 
- Directory of D:\Class\Assignments\DomChallenge
 
-09/21/2018  04:55 PM    <DIR>          .
-09/21/2018  04:55 PM    <DIR>          ..
-09/21/2018  04:55 PM               409 index.css
-09/21/2018  04:55 PM               359 index.html
-09/21/2018  04:55 PM                 0 index.js
-               3 File(s)            768 bytes
-               2 Dir(s)  992,023,048,192 bytes free
+function buttonOneWordChanger(){
+	buttonTwoWordChanger.style.fot
+}
+function buttonTwoWordChanger(){
+	buttonOneText.style.color = "white";
+	buttonTwoText.style.color = "blue"
+}
